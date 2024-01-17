a275292095931752bf781717b4867629b150dd45
이정관 <wellsbabo@gravity.co.kr>
2024-01-17 10:22:51 +0900

    오류 해결 원인: 경로 문제, 설정 파일 문제, 엣지로 실행

--- servlet/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -5,16 +5,17 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
-@WebServlet(name="helloServlet", urlPatterns = "/hello")
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         System.out.println("HelloServlet.service");
         System.out.println("request = " + request);
-        System.out.println("request = " + request);
+        System.out.println("response = " + response);
 
         String username = request.getParameter("username");
         System.out.println("username = " + username);
@@ -22,5 +23,6 @@ public class HelloServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
         response.getWriter().write("hello " + username);
+
     }
 }
